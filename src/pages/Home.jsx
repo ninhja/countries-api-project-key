25d0746ce1e4b555a2af5529b3c88cce1726d465
ninhja <ariannaninh@gmail.com>
2025-01-08 17:47:00 -0800

    added My Profile form, added search bar on home page, added API call and displayed countries data from API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,57 @@
 import data from "../../data.json";
 import Card from "../components/Card";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [error, setError] = useState(null);
+
+  const getCountriesData = () => {
+    fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flags,population,capital,region"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const sortedData = data.sort((a, b) =>
+          a.name.common.toLowerCase().localeCompare(b.name.common.toLowerCase())
+        );
+        setCountries(sortedData);
+        setFilteredCountries(sortedData);
+      })
+      .catch((error) => setError("Error: " + error.message));
+  };
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+
+    if (searchTerm.trim() === "") {
+      // Reset to full countries list if input is empty
+      setFilteredCountries(countries);
+    } else {
+      const filtered = countries.filter((item) =>
+        item.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredCountries(filtered);
+    }
+  };
+
+  useEffect(() => {
+    getCountriesData();
+  }, []);
   return (
     <main>
-      <h2>Home</h2>
+      <div class="menu">
+        <input
+          name="searchBar"
+          type="text"
+          placeholder="Search for a country..."
+          onChange={handleSearch}
+        ></input>
+      </div>
 
       <div className="card-container">
-        {data.map((country) => (
+        {filteredCountries.map((country) => (
           <Card
             key={country.name.common}
             name={country.name.common}
--- src/pages/SavedCountries.jsx
+++ src/pages/SavedCountries.jsx
@@ -1,7 +1,65 @@
+import { useState } from "react";
+
 export default function SavedCountries() {
+  const emptyFormState = { fullName: "", email: "", country: "", bio: "" };
+  const [formData, setFormData] = useState(emptyFormState);
+  const [userInfo, setUserInfo] = useState(emptyFormState);
+
+  // Update the state when input values change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // if (!formData.e.target.value) {
+    //   alert("Please enter your full name.");
+    //   return;
+    // }
+    setUserInfo(formData);
+    setFormData(emptyFormState); // reset formData
+  };
+
   return (
     <main>
-      <h2>Saved Countries Page</h2>
+      <h2>My Saved Countries</h2>
+
+      <section className="form-wrapper">
+        <h2>My Profile</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleInputChange}
+            placeholder="Full name"
+          ></input>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Email"
+          ></input>
+          <input
+            type="text"
+            name="country"
+            value={formData.country}
+            onChange={handleInputChange}
+            placeholder="Country"
+          ></input>
+          <input
+            type="text"
+            name="bio"
+            value={formData.bio}
+            onChange={handleInputChange}
+            placeholder="Bio"
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </section>
     </main>
   );
 }
